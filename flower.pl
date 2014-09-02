3e177d46c3eb64ff13dd0b83da8bede22c282d97
tammurmas <tammurmas@gmail.com>
2014-09-02 16:29:46 +0300

    1st Prolog assignment
    Solution for the flower/1 predicate.

---
+++ flower.pl
@@ -0,0 +1,46 @@
+% Prolog predicate flower/1 that draws a flower using ASCII symbols.
+%
+% The parameter N scales the drawing.
+%
+% Author: Urmas Tamm, a21920
+%
+
+flower(X):-
+	X > 0,
+	upper_lower(X),
+	middle(X,X),
+        upper_lower(X),
+	bars(X),
+	!.%disallow backtracking, we're done here
+
+upper_lower(X):-
+	space(X),
+	zero(X),
+	space(X),
+	nl.
+
+middle(X,Y):-
+	Y > 0,
+	Y1 is Y-1,
+	zero(X),
+	space(X),
+	zero(X),
+	nl,
+	middle(X,Y1).
+middle(_,0).
+
+
+bars(X):-
+	space(X),
+	bar(X),
+	space(X).
+
+space(X):-X > 0, write(' '), X1 is X-1, space(X1).
+space(0).
+
+zero(X):-X > 0, write('0'), X1 is X-1, zero(X1).
+zero(0).
+
+bar(X):-X > 0, write('|'), X1 is X-1, bar(X1).
+bar(0).
+
